81adb7a075b0f82c786f63a5b6bbfc206aec24f4
Bernard Blackham <bernard@tuxonice.net>
2006-12-19 10:58:19 +0000

    script for generating tarballs.

--- hibernate-tarball.sh
+++ hibernate-tarball.sh
@@ -6,6 +6,6 @@ FULLNAME=hibernate-script-$1
 
 [ -e $FULLNAME -o -e $FULLNAME.tar.gz ] && echo "Already exists!" && exit
 
-cp -a tags/release-$1 $FULLNAME
+! cp -a tags/release-$1 $FULLNAME && echo "No such release?" && exit 1
 fakeroot tar --exclude '.svn' -czvf $FULLNAME.tar.gz $FULLNAME
 rm -rf "$FULLNAME"
